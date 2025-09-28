f2908eef5aa2e9d0a2767e14709bce6fd8cba3d2
Viorel Budeci <viorel.budeci@cegeka.com>
2025-09-28 20:19:58 +0300

    change location for static web app

--- infrastructure/terraform/modules/environment/static-web-app.tf
+++ infrastructure/terraform/modules/environment/static-web-app.tf
@@ -1,5 +1,5 @@
-resource "azurerm_static_web_app" "vimusic_ui" {
+resource "azurerm_static_web_app" "webapp" {
   name                = "stapp-${var.application_name}-${var.environment_name}-${var.region_identifier}"
   resource_group_name = azurerm_resource_group.rg_env.name
-  location            = azurerm_resource_group.rg_env.location
+  location            = "westeurope" //data.azurerm_resource_group.this.location
 }
