28a19e7a7fe97296eafe82bf74df60afd88db716
Viorel Budeci <viorel.budeci@cegeka.com>
2025-10-01 00:26:37 +0300

    add small comment

--- infrastructure/terraform/modules/environment/web-app.tf
+++ infrastructure/terraform/modules/environment/web-app.tf
@@ -13,7 +13,7 @@ resource "azurerm_linux_web_app" "webapp" {
   service_plan_id     = azurerm_service_plan.plan.id
 
   identity {
-    type         = "UserAssigned"
+    type         = "UserAssigned" # add SystemAssigned if not works (web app + user assigned => bug in azure on reading key vault reference?)
     identity_ids = [azurerm_user_assigned_identity.uami.id]
   }
 
